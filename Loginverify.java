63ebe32792af47fc74592e566e2093bd9ca4a0ba
KRISHNA2739 <68938528+KRISHNA2739@users.noreply.github.com>
2020-08-12 19:27:17 +0530

    Add files via upload

---
+++ Loginverify.java
@@ -0,0 +1,91 @@
+package krishna;
+
+import java.io.IOException;
+import java.sql.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+ 
+
+public class Loginverify extends HttpServlet {
+ 
+   
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		HttpSession session=request.getSession();
+		Connection myConn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		if(username.isEmpty() || password.isEmpty() )
+		{
+			response.sendRedirect("Loginpage.jsp");
+			//RequestDispatcher req = request.getRequestDispatcher("Loginpage.jsp");
+			//req.include(request, response);
+		}
+		else
+		{
+			
+		boolean status=false;
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String user = "client";		
+		String pass = "client";
+	
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			
+		 myConn = DriverManager.getConnection(dbUrl, user, pass);
+
+
+			//PreparedStatement ps=con.prepareStatement("Select * from MAILCASTINGUSER where EMAILADD = ? and PASSWORD =?");
+			 ps=myConn.prepareStatement("Select accountno from accountsecure where username = ? and password =?");
+
+			ps.setString(1,username);
+			ps.setString(2,password);
+			
+			rs=ps.executeQuery();
+			status=rs.next();
+			}
+			catch (Exception exc) {
+				exc.printStackTrace();
+			}
+			
+			
+			
+			
+			
+
+
+		if(status)
+		{
+			String accountno=null;
+			try {
+				accountno = rs.getString("accountno");
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+			
+			session.setAttribute("accountno",accountno);
+			response.sendRedirect("Abs.jsp");
+			
+			//RequestDispatcher req = request.getRequestDispatcher("Abs.jsp");
+			//req.forward(request, response);
+		}
+		else
+		{
+			response.sendRedirect("Loginpage.jsp");
+		}
+		}
+
+	}
+ 
+}
