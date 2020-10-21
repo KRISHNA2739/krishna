94c2b3d9e7e004d504bc04671a05b7eee769bdd7
KRISHNA2739 <68938528+KRISHNA2739@users.noreply.github.com>
2020-10-21 10:06:00 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -8,17 +8,10 @@ public class q5 {
 	
 	public static void main(String[] args) {
 		int[] a = new int[] {21, 22 ,23,24,25}; 
-	    
-	  
-	    
-	   
-	     int[] a1 = Arrays.copyOf(a, 5); 
-	  
-	   
-	     for (int i = 0; i < a1.length; i++)  
-	         System.out.print(a1[i] + " "); 
-
-	}
+	         int[] a1 = Arrays.copyOf(a, 5); 
+	        for (int i = 0; i < a1.length; i++)  
+	               System.out.print(a1[i] + " "); 
+}
 	}
 
 
